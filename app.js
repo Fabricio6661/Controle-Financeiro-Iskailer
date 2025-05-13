f86690d750ddbac70dc913f5aaa6ca900920b9f8
fabricio_dalanhol <fabricio_dalanhol@estudante.sesisenai.org.br>
2025-05-12 21:56:07 -0300

    denovo

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ const form = document.getElementById('RecordForm');
 const list = document.getElementById('RecordList');
 const TotalDaily = document.getElementById('DailySum');
 const Total = document.getElementById('TotalSum');
-const today = new Date().toISOString().slice(0, 10);
+const today = new Date().toISOString().slice(0,10);
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const desc = document.getElementById('Descrição').value;
-    const value = document.getElementById('Valor').value;
-    const recordtype = document.getElementById('Type').value;
+    const desc = document.getElementById('Description').value;
+    const value = parseFloat(document.getElementById('Value').value);
+    const recordType = document.getElementById('Type').value;
     const date = today;
-    
+
     const doc = {
         _id: new Date().toISOString(),
-        desc, value, recordtype, date
+        desc,value,recordType,date
+
     };
 
     await db.put(doc);
     form.reset();
     refresh();
-});
+}
+);
 
 async function refresh() {
     const res = await db.allDocs({include_docs: true, descending: true});
@@ -30,12 +32,11 @@ async function refresh() {
     let totalG = 0;
     
     res.rows.forEach(row => {
-        const {desc, value, recordtype, date} = row.doc;
+        const {desc,value,recordType,date} = row.doc;
         const li = document.createElement('li');
-        li.textContent = `${date} - ${desc} : R$ ${value.toFixed(2)} - (${recordtype})`;
-        pendChild(li);
-    }
-    )
-}
+        li.textContent = `${date} - ${desc} : R${value.toFixed(2)} - (${recordType})`;
+        list.appendChild(li);
+                
+    });
 
-refresh();
+}
