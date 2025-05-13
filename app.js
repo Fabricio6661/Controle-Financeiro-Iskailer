1f5679f9791b1804edc0374eb45e613e4f47049d
fabricio_dalanhol <fabricio_dalanhol@estudante.sesisenai.org.br>
2025-05-12 21:46:33 -0300

    criando css js

---
+++ app.js
@@ -0,0 +1,41 @@
+const db = new PouchDB('Finances');
+const form = document.getElementById('RecordForm');
+const list = document.getElementById('RecordList');
+const TotalDaily = document.getElementById('DailySum');
+const Total = document.getElementById('TotalSum');
+const today = new Date().toISOString().slice(0, 10);
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const desc = document.getElementById('Descrição').value;
+    const value = document.getElementById('Valor').value;
+    const recordtype = document.getElementById('Type').value;
+    const date = today;
+    
+    const doc = {
+        _id: new Date().toISOString(),
+        desc, value, recordtype, date
+    };
+
+    await db.put(doc);
+    form.reset();
+    refresh();
+});
+
+async function refresh() {
+    const res = await db.allDocs({include_docs: true, descending: true});
+    list.innerHTML = '';
+    let totalD = 0;
+    let totalG = 0;
+    
+    res.rows.forEach(row => {
+        const {desc, value, recordtype, date} = row.doc;
+        const li = document.createElement('li');
+        li.textContent = `${date} - ${desc} : R$ ${value.toFixed(2)} - (${recordtype})`;
+        pendChild(li);
+    }
+    )
+}
+
+refresh();
